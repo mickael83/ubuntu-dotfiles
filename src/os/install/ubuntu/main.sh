eac18c02d6e1301bd63a417bd96a573cc3f34fe0
Michael Pinheiro <mickael.pinheiro@gmail.com>
2017-04-13 00:45:20 +0100

    Fix node installation

--- src/os/install/ubuntu/main.sh
+++ src/os/install/ubuntu/main.sh
@@ -97,10 +97,14 @@ install_databases() {
     echo "deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-tools" | sudo tee --append /etc/apt/sources.list.d/mysql.list > /dev/null
     update
 
-    echo "mysql-server-5.6 mysql-server/root_password password root" | sudo debconf-set-selections
-    echo "mysql-server-5.6 mysql-server/root_password_again password root" | sudo debconf-set-selections
+    echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
+    echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
     install_package "MySQL" "mysql-server"
-    execute "secure_mysql" "Secure MySQL"
+
+    # Run the MySQL Secure Installation wizard
+    execute "mysql_secure_installation" "Secure MySQL"
+    sudo sed -i 's/127\.0\.0\.1/0\.0\.0\.0/g' /etc/mysql/my.cnf
+    mysql -uroot -p -e 'USE mysql; UPDATE `user` SET `Host`="%" WHERE `User`="root" AND `Host`="localhost"; DELETE FROM `user` WHERE `Host` != "%" AND `User`="root"; FLUSH PRIVILEGES;'
 
     install_package "MongoDB" "mongodb"
 }
